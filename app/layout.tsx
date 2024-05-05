2ff12d25c6818442dea2b5fd6e42d32b2cd0f424
waylonwilliams <crazyocean9@gmail.com>
2024-05-04 21:25:23 -0700

    init proj

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
-import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+
+// UPDATE MEE global font
+import { Poppins } from "next/font/google";
+const globalFont = Poppins({ subsets: ["latin"], weight: "300" });
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
-
+// UPDATE MEE page metadata
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Waylon next js template",
+  description: "Template",
 };
 
 export default function RootLayout({
@@ -17,12 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
-      </body>
+    <html lang="en">
+      <body className={globalFont.className}>{children}</body>
     </html>
   );
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { headers } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
 import { redirect } from "next/navigation";
-import { SubmitButton } from "./submit-button";
+import { SubmitButton } from "../../components/SubmitButton";
 
 export default function Login({
   searchParams,
@@ -52,67 +52,36 @@ export default function Login({
   };
 
   return (
-    <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
-      <Link
-        href="/"
-        className="absolute left-8 top-8 py-2 px-4 rounded-md no-underline text-foreground bg-btn-background hover:bg-btn-background-hover flex items-center group text-sm"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
-        >
-          <polyline points="15 18 9 12 15 6" />
-        </svg>{" "}
-        Back
-      </Link>
+    <div>
+      <div>
+        <Link href="/">Back</Link>
+      </div>
 
-      <form className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-        <label className="text-md" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          name="email"
-          placeholder="you@example.com"
-          required
-        />
-        <label className="text-md" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="rounded-md px-4 py-2 bg-inherit border mb-6"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          required
-        />
-        <SubmitButton
-          formAction={signIn}
-          className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing In..."
-        >
+      <form>
+        <div>
+          <label className="text-md" htmlFor="email">
+            Email
+          </label>
+          <input name="email" placeholder="you@example.com" required />
+        </div>
+        <div>
+          <label className="text-md" htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            placeholder="••••••••"
+            required
+          />
+        </div>
+        <SubmitButton formAction={signIn} pendingText="Signing In...">
           Sign In
         </SubmitButton>
-        <SubmitButton
-          formAction={signUp}
-          className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
-          pendingText="Signing Up..."
-        >
+        <SubmitButton formAction={signUp} pendingText="Signing Up...">
           Sign Up
         </SubmitButton>
-        {searchParams?.message && (
-          <p className="mt-4 p-4 bg-foreground/10 text-foreground text-center">
-            {searchParams.message}
-          </p>
-        )}
+        <div>{searchParams?.message && <p>{searchParams.message}</p>}</div>
       </form>
     </div>
   );
