9111d8374bb94d01a6d76aa6bbdb0feadd72745d
waylonwilliams <crazyocean9@gmail.com>
2024-05-10 19:49:52 -0700

    more more styling

--- components/CreateToDo.jsx
+++ components/CreateToDo.jsx
@@ -2,18 +2,24 @@ import { useState } from "react";
 import { createClient } from "@/utils/supabase/client";
 
 export default function CreateToDo({ todos, setTodos }) {
-  const [title, setTitle] = useState("");
-  const [description, setDescription] = useState("");
-  const [date, setDate] = useState("");
+  // const [title, setTitle] = useState("");
+  // const [description, setDescription] = useState("");
+  // const [date, setDate] = useState("");
 
   const supabase = createClient();
 
-  async function insertTodo() {
+  async function insertTodo(event) {
+    event.preventDefault();
+
+    const title = event.target.title.value;
+    const description = event.target.description.value;
+    const date = event.target.date.value;
+
     const { data, error } = await supabase
       .from("todosnew")
       .insert([
         {
-          title: title == "" ? null : title,
+          title: title,
           description: description == "" ? null : description,
           due_date: date == "" ? null : date,
           complete: false,
@@ -27,9 +33,6 @@ export default function CreateToDo({ todos, setTodos }) {
       return;
     }
     setTodos([...todos, data[0]]);
-    setDate("");
-    setDescription("");
-    setTitle("");
     const dialog = document.getElementById("todo_modal");
     if (dialog) dialog.close();
   }
@@ -50,62 +53,62 @@ export default function CreateToDo({ todos, setTodos }) {
         <div className="modal-box">
           <h3 className="font-bold text-lg">New to-do</h3>
 
-          <label htmlFor="title" className="block mt-2 text-sm text-gray-600">
-            Title
-          </label>
-          <input
-            id="title"
-            name="title"
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="w-full px-2 py-1 border rounded-lg mt-1"
-          />
+          <form onSubmit={insertTodo}>
+            <label htmlFor="title" className="block mt-2 text-sm text-gray-600">
+              Title
+            </label>
+            <input
+              id="title"
+              name="title"
+              type="text"
+              required
+              // value={title}
+              // onChange={(e) => setTitle(e.target.value)}
+              className="w-full px-2 py-1 border rounded-lg mt-1"
+            />
 
-          <label
-            htmlFor="description"
-            className="block mt-2 text-sm text-gray-600"
-          >
-            Description
-          </label>
-          <textarea
-            id="description"
-            name="description"
-            type="text"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="w-full px-2 py-1 border rounded-lg mt-1"
-            rows={4}
-          />
+            <label
+              htmlFor="description"
+              className="block mt-2 text-sm text-gray-600"
+            >
+              Description
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              type="text"
+              // value={description}
+              // onChange={(e) => setDescription(e.target.value)}
+              className="w-full px-2 py-1 border rounded-lg mt-1"
+              rows={4}
+            />
 
-          <label htmlFor="date" className="block mt-2 text-sm text-gray-600">
-            Complete by
-          </label>
-          <input
-            id="date"
-            name="date"
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-            className="w-full px-2 py-1 border rounded-lg mt-1"
-          />
+            <label htmlFor="date" className="block mt-2 text-sm text-gray-600">
+              Complete by
+            </label>
+            <input
+              id="date"
+              name="date"
+              type="date"
+              // value={date}
+              // onChange={(e) => setDate(e.target.value)}
+              className="w-full px-2 py-1 border rounded-lg mt-1"
+            />
 
-          <div className="flex justify-center">
-            <button className="btn btn-primary mt-5" onClick={insertTodo}>
-              Create
-            </button>
-          </div>
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="btn btn-primary mt-5"
+                // onClick={insertTodo}
+              >
+                Create
+              </button>
+            </div>
+          </form>
 
           <div className="modal-action">
             <form method="dialog">
-              <button
-                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
-                onClick={() => {
-                  setDate("");
-                  setDescription("");
-                  setTitle("");
-                }}
-              >
+              <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
                 ✕
               </button>
             </form>
--- components/ToDoCard.tsx
+++ components/ToDoCard.tsx
@@ -1,4 +1,5 @@
 import { ToDoType } from "./ToDoTypes";
+import Image from "next/image";
 
 interface Props {
   todo: ToDoType;
@@ -6,21 +7,39 @@ interface Props {
 
 export default function ToDoCard({ todo }: Props) {
   return (
-    <div className="w-1/2 md:w-4/5 bg-base-100 shadow-xl p-6 rounded-lg m-2">
-      <div className="flex">
+    <div className="w-1/2 bg-base-100 shadow-xl p-6 rounded-lg m-2 relative">
+      <div className="flex items-center">
         <h2
           className={
-            "font-bold text-xl" + (todo.complete ? " text-base-300" : "")
+            "font-bold text-xl" +
+            (todo.complete ? " text-base-300" : "") +
+            (todo.description == null ? " mb-6" : "")
           }
         >
           {todo.title}
         </h2>
-        <h6 className="">{todo.due_date}</h6>
+        <h6 className="ml-5">Due: {todo.due_date}</h6>
       </div>
-      <p className={todo.complete ? "text-gray-200" : ""}>{todo.description}</p>
-      <div className="card-actions justify-end">
-        <button className="btn btn-primary">Button</button>
+      <p className={"w-4/5" + (todo.complete ? " text-gray-200" : "")}>
+        {todo.description}
+      </p>
+
+      {/* COMPLETE  */}
+      <div className="card-actions absolute right-2 bottom-3">
+        <button className="btn btn-sm btn-circle btn-primary">
+          <Image
+            src={"/images/check.svg"}
+            alt="Complete"
+            width={30}
+            height={30}
+          />
+        </button>
       </div>
+
+      {/* EDIT */}
+      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+        <Image src="/images/pencil.png" alt="Edit" width={20} height={20} />
+      </button>
     </div>
   );
 }
